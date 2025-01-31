0a5efa03ab1221fbc76324430b7b9c5d713fe29c
yellowdollar <yellowdollar55@gmail.com>
2025-01-31 18:58:44 +0500

    Push

---
+++ eng/static/js/OpenMobileLang.js
@@ -0,0 +1,4 @@
+let lang_menu = document.querySelector('.dropdown_lang_mobile .dropdown_menu');
+document.querySelector('.dropdown_lang_mobile').addEventListener('click', function() {
+    lang_menu.classList.toggle('show');
+});
