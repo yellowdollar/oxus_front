9b235cadcd504941b588721c58ed88510edf2e39
yellowdollar <yellowdollar55@gmail.com>
2025-02-12 16:34:52 +0500

    push

---
+++ static/js/NotificationShow.js
@@ -0,0 +1,17 @@
+const notification = document.querySelector(".push_notifications .notification");
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(() => {
+        notification.classList.toggle("show");
+    }, 1000); // 1000 миллисекунд = 1 секунда
+});
+
+notification.addEventListener('click', function() {
+    notification.classList.add("show");
+});
+
+const exit_notification = document.querySelector(".push_notifications .notification .exit");
+
+exit_notification.addEventListener('click', function() {
+    event.stopPropagation(); 
+    notification.classList.remove('show');
+})
